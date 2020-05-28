1e813b67ea6e98c035376b8fc90031fbc1e75add
Jerome Michel <micl.jerome@gmail.com>
2020-05-28 15:33:43 +0200

    Create cocktail ingredient dose models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,7 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
-  LIST = ['Mint Julep', 'Whiskey Sour', 'Mojito']
-
-  validates :name, uniqueness: true, inclusion: { in: LIST }
+  validates :name, uniqueness: true, presence: true
 end
