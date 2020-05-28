749109547c9a7c41694a204400ae36a0bb784010
Jerome Michel <micl.jerome@gmail.com>
2020-05-28 16:50:37 +0200

    Create cocktails controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 puts "Cleaning DB..."
 Ingredient.destroy_all
+Cocktail.destroy_all
 
 puts "Creating ingredients..."
 Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
+Cocktail.create(name: 'pritz')
+Cocktail.create(name: 'tagada sprite')
+Cocktail.create(name: 'coca-zoula')
 
 puts "Created #{Ingredient.count} ingredients"
+puts "Created #{Cocktail.count} cocktails"
+
