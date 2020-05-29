54ae0b66109a139379722598269a40dc78e8b52a
Jerome Michel <micl.jerome@gmail.com>
2020-05-29 15:45:41 +0200

    Mister Cocktail ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,6 +4,26 @@ class CocktailsController < ApplicationController
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
     @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to @cocktail, notice: 'Cocktail was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index] do
+  resources :cocktails, only: [:index, :show, :new, :create] do
     resources :ingredients, only: [:show]
+    resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [:destroy]
 end
